5c83e63e7981220b710ef947df0212b48ce1ce51
shukhrat <sadullayevsh24@gmail.com>
2023-04-26 12:08:42 +0500

    hi

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-$("h1").css("color", "purple");
+// $("h1").click(function () {
+//   $("h1").css("color", "purple");
+//   $("h1").css("font-size", "10rem");
+// });
+// $("a").attr("href", "https://www.bing.com");
+
+// $(document).keypress(function (event) {
+//   $("h1").text(event.key);
+// });
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "purple");
+});
